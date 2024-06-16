5d1623ec96ec331e4ea698b3c23cf91af96e6c97
aniket <aniket.umbc@gmail.com>
2024-06-16 12:20:36 +0530

    Added screen guard for routes

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [msg, setMsg] = useState('');
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{msg}</Text>
     </View>
   );
 }
